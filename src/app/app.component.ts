c9fe7da946fb7783a3d8cc6a1f66eb3e5493b218
Akshay-Kumar-Singh <akshayrana707@gmail.com>
2019-03-12 03:54:40 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'alliance';
+  openNav() {
+    document.getElementById('mySidenav').style.width = '250px';
+  }
+  closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+  }
+}
